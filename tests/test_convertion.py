d65b4f22880798601a5388d6ab952c13e621ab75
evacuervosolis <evacuervosolis@gmail.com>
2025-03-21 11:53:16 +0100

    Ejecutamos test con pytest

---
+++ tests/test_convertion.py
@@ -0,0 +1,16 @@
+import pytest
+from src.t8_client.module import convert_unix_to_iso
+
+def test_convert_unix_to_iso():
+    timestamp = 1609459200  
+    iso_string = convert_unix_to_iso(timestamp)
+    assert iso_string == "2021-01-01T00:00:00", f"Expected '2021-01-01T00:00:00', got {iso_string}"
+    
+    timestamp = 0  
+    iso_string = convert_unix_to_iso(timestamp)
+    assert iso_string == "1970-01-01T00:00:00", f"Expected '1970-01-01T00:00:00', got {iso_string}"
+
+
+
+
+
